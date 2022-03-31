bbe7c3fac4f4c657a9af2b85bc6b7a8d55af0c21
Ahmad Mahmood <23100146@lums.edu.pk>
2022-04-01 01:22:46 +0500

    sign up route done

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -10,21 +10,41 @@ router.post('/signup', async (req,res) => {
         const usernames = await pool.query(
             "SELECT username FROM users WHERE username = $1", [username]  
         );
-        if (usernames.rows != []){
+        if (usernames.rowCount > 0){
             res.status(400).json({ error: `An account with this username exists. Please try again with a different username.` })
         }
         else{
             try {
-                
-                const email = await pool.query(
-                    "SELECT email FROM users WHERE email = $1", [username]  
+                const { email } = req.body.email;
+                const emails = await pool.query(
+                    "SELECT email FROM users WHERE email = $1", [email]  
                 );
+                if (emails.rowCount > 0){
+                    res.status(400).json({ error: `An account with this email exists. Please try again with a different email.` })
+                }
+                else{
+                    try {
+                        
+                        const result = await pool.query(
+                            "INSERT INTO users(username,password,fullname,email,question_field,answer) VALUES($1,$2,$3,$4,$5,$6);", [req.body.username,req.body.password,req.body.fullname,req.body.email,req.body.question,req.body.answer]
+
+                        );
+                        console.log("signed up");
+                        res.status(200).json({})
+
+                    } catch (err) {
+                        console.log(err);
+                        res.status(400).json({ error: `Request failed. Try again.` })
+                    }
+                }
 
             } catch (err) {
+                console.log(err);
                 res.status(400).json({ error: `Request failed. Try again.` })
             }
         }
     } catch (err) {
+        console.log(err);
         res.status(400).json({ error: `Request failed. Try again.` })
         
     }
