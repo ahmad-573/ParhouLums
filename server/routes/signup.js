d84947dc12c1c06750e0bb2dc994f97d5f30badd
Ahmad Mahmood <23100146@lums.edu.pk>
2022-03-31 23:54:25 +0500

    signup router started

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -2,4 +2,16 @@ const express = require(`express`)
 const router = express.Router()
 const pool = require("../db");
 
-
+// Sign up post route
+router.post('/signup', async (req,res) => {
+    try {
+        const { username } = req.body.username;
+        const usernames = await pool.query(
+            "SELECT username FROM users WHERE username = $1", [username]  
+        );
+        if ()
+    } catch (err) {
+        console.error(err.message);
+        
+    }
+})
