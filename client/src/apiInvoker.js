aaf2b3b203da151002acdcf215f4bea26ed4342e
Saad Sheikh <23100138@lums.edu.pk>
2022-04-05 00:57:20 +0500

    add: Added apiInvoker method

---
+++ client/src/apiInvoker.js
@@ -0,0 +1,34 @@
+export async function apiInvoker(api, body = {}, sendToken = true) {
+  try {
+    let reqHeader = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+
+    if (sendToken) {
+      body.token = localStorage.token
+    }
+
+    if (body !== {}) {
+      req_init['body'] = JSON.stringify(body)
+    }
+
+    const res = await fetch(api, reqHeader)
+    if (res.ok) {
+      const data = await res.json()
+      
+      if (data.statusCode !== successCode) {
+        throw new Error(data.error)
+      }
+      return data
+    }
+
+    throw new Error("" + res.status + res.statusText) 
+  }
+  catch (err) {
+    return err.message
+  }
+}
