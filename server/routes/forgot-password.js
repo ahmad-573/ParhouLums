63ce451d484d72d35ebe59e88fd7a1f25c44d30f
Ahmad Mahmood <23100146@lums.edu.pk>
2022-04-05 01:49:06 +0500

    forgot password

--- server/routes/forgot-password.js
+++ server/routes/forgot-password.js
@@ -4,7 +4,30 @@ const pool = require("../db");
 
 // Sign up post route
 router.post('/forgot-password', async (req,res) => {
-    
+    try {
+        const { email } = req.body.email;
+        const emails = await pool.query(
+            "SELECT email FROM users WHERE email = $1", [email]  
+        );
+        if (emails.rowCount === 0){
+            res.status(400).json({ error: `Wrong email. Please enter the correct email address` })
+        }
+        else{
+            try {
+                const { question } = req.body.question;
+                const questions = await pool.query(
+                    "SELECT question_field FROM users WHERE email = $1", [email]  
+        );
+            } catch (err) {
+                console.log(err);
+                res.status(400).json({ error: `Request failed. Try again.` })
+            }
+        }
+        
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ error: `Request failed. Try again.` })
+    }
 })
 
 module.exports = router;
