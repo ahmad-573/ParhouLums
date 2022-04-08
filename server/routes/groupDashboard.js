15ff521d687a35d33c2840efd7b9a627bf6b484a
Ahmad Mahmood <23100146@lums.edu.pk>
2022-04-09 03:03:37 +0500

    testing auth

--- server/routes/groupDashboard.js
+++ server/routes/groupDashboard.js
@@ -5,7 +5,7 @@ const pool = require("../db");
 // Get all groups of a user
 router.post('/getAllGroups', async (req,res) => {
     try {
-        let user_id = 7;  // req.body.userid
+        let user_id = req.body.userid;
         const user_groups = await pool.query(
             "SELECT group_name AS name, status, g.group_id FROM groups AS g INNER JOIN group_membership AS gm ON g.group_id = gm.group_id WHERE gm.user_id = $1 ", [user_id]
         );
