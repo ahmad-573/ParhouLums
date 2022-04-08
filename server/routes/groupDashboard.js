18b5cd7f32d9b8de93eae4fac023ea445c91107a
Ahmad Mahmood <23100146@lums.edu.pk>
2022-04-09 00:56:15 +0500

    restart: groupDashboard

--- server/routes/groupDashboard.js
+++ server/routes/groupDashboard.js
@@ -6,16 +6,7 @@ const pool = require("../db");
 router.post('/getAllGroups', (req,res) => {
     const { user_name } = req.body.username;
     try {
-        const user_ids = await pool.query(
-            "SELECT user_id FROM users WHERE username = $1", [user_name]  
-        );
-        const user_id = user_ids.rows[0];
-        try {
-           
-        } catch (err) {
-            console.log(err);
-            res.status(400).json({ error: `Request failed. Try again.` })
-        }
+        
     } catch (err) {
         console.log(err);
         res.status(400).json({ error: `Request failed. Try again.` })
