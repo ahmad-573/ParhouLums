29722530184c160eaa92ee590c2b565371bd8248
Ahmad Mahmood <23100146@lums.edu.pk>
2022-04-12 12:18:57 +0500

    links done

--- server/routes/link.js
+++ server/routes/link.js
@@ -3,14 +3,14 @@ const router = express.Router()
 const pool = require("../db");
 const helpers = require('./helper.js');
 
-// Get all topics
-router.post('/getTopics', async (req,res) => {
+// Get all links
+router.post('/getLinks', async (req,res) => {
     if (await helpers.isUserInGroup(res,req.body.userid, req.body.group_id)){
         try {
             const result = await pool.query(
-                "SELECT topic_id, title FROM topics WHERE group_id = $1", [req.body.group_id]
+                "SELECT link_id, link FROM links WHERE topic_id = $1", [req.body.topic_id]
             );
-            res.status(200).json({topics: result.rows})
+            res.status(200).json({links: result.rows})
         } catch (err) {
             console.log(err);
             res.status(400).json({error: `Request failed. Try again`})
@@ -18,12 +18,12 @@ router.post('/getTopics', async (req,res) => {
     }
 });
 
-// Create a topic
-router.post('/addTopic', async (req,res) => {
+// Create a link
+router.post('/addLink', async (req,res) => {
     if (await helpers.isUserInGroup(res,req.body.userid, req.body.group_id)){
         try {
             const result = await pool.query(
-                "INSERT INTO topics(group_id,title) VALUES ($1,$2) ", [req.body.group_id, req.body.title]
+                "INSERT INTO links(topic_id,link) VALUES ($1,$2) ", [req.body.topic_id, req.body.link]
             );
             res.status(200).json({})
         } catch (err) {
@@ -33,27 +33,12 @@ router.post('/addTopic', async (req,res) => {
     }
 });
 
-// Delete a topic 
-router.post('/deleteTopic', async (req,res) => {
+// Delete a link
+router.post('/deleteLink', async (req,res) => {
     if (await helpers.isUserInGroup(res,req.body.userid, req.body.group_id)){
         try {
             const result = await pool.query(
-                "DELETE FROM topics WHERE topic_id = $1 AND group_id = $2", [req.body.topic_id, req.body.group_id]
-            );
-            res.status(200).json({})
-        } catch (err) {
-            console.log(err);
-            res.status(400).json({error: `Request failed. Try again.`})
-        }
-    }
-});
-
-// edit a topic
-router.post('/editTopic', async (req,res) => {
-    if (await helpers.isUserInGroup(res,req.body.userid, req.body.group_id)){
-        try {
-            const result = await pool.query(
-                "UPDATE topics SET title = $1 WHERE topic_id = $2 AND group_id = $3", [req.body.new_title, req.body.topic_id, req.body.group_id]
+                "DELETE FROM links WHERE link_id = $1 AND topic_id = $2", [req.body.link_id, req.body.topic_id]
             );
             res.status(200).json({})
         } catch (err) {
