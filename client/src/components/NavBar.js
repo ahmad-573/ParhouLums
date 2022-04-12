cf242171d05ca4e6b02346335f2bdf8557803ac8
Saad Sheikh <23100138@lums.edu.pk>
2022-04-12 21:57:23 +0500

    catered Token Error in NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -110,7 +110,12 @@ function NavBar({navTitle, setNavTitle, setGroup, logout, setSnackbarMsg, setGro
         setMemberMap(newMemberMap)
         setMemberList(newMemberList)
       } else {
-        setSnackbarMsg('Create Group Error: ' + err)
+        if (err === 'Token error') {
+          logout()
+          navigate('/', { replace: true })
+        } else {
+          setSnackbarMsg('Create Group Error: ' + err)
+        }
       }
     })
 
@@ -171,7 +176,12 @@ function NavBar({navTitle, setNavTitle, setGroup, logout, setSnackbarMsg, setGro
               if (err === undefined) {
                 setGroups([...groups, {name: values.groupName, group_id: data.group_id, status: 1}])
               } else {
-                setSnackbarMsg('Create Group Error: ' + err)
+                if (err === 'Token error') {
+                  logout()
+                  navigate('/', { replace: true })
+                } else {
+                  setSnackbarMsg('Create Group Error: ' + err)
+                }
               }
             }}
             >
