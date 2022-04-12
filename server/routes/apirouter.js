9a61e28a0e306ea6c0b28d7cb92cfc7e15cf2af3
Fahad-Touseef <23100032@lums.edu.pk>
2022-04-13 00:33:09 +0500

    Task list functionalities added in apirouter.js

--- server/routes/apirouter.js
+++ server/routes/apirouter.js
@@ -19,5 +19,11 @@ router.use('/api', require('./removeParticipants'));
 router.use('/api', require('./promoteToAdmin'));
 router.use('/api', require('./renameGroup'));
 router.use('/api', require('./leaveGroup'));
+router.use('/api', require('./createTask'));
+router.use('/api', require('./getTasks'));
+router.use('/api', require('./editTask'));
+router.use('/api', require('./deleteTask'));
+router.use('/api', require('./moveTask'));
+
 
 module.exports = router;
