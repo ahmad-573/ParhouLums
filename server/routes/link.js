e12815d4b37f8d97fe1d6dc0ae2a0a86a15f8b12
Ahmad Mahmood <23100146@lums.edu.pk>
2022-04-13 16:31:56 +0500

    link apis updated

--- server/routes/link.js
+++ server/routes/link.js
@@ -8,7 +8,7 @@ router.post('/getLinks', async (req,res) => {
     if (await helpers.isUserInGroup(res,req.body.userid, req.body.group_id)){
         try {
             const result = await pool.query(
-                "SELECT link_id, link FROM links WHERE topic_id = $1", [req.body.topic_id]
+                "SELECT link_id, link, title FROM links WHERE topic_id = $1", [req.body.topic_id]
             );
             res.status(200).json({links: result.rows})
         } catch (err) {
@@ -23,7 +23,7 @@ router.post('/addLink', async (req,res) => {
     if (await helpers.isUserInGroup(res,req.body.userid, req.body.group_id)){
         try {
             const result = await pool.query(
-                "INSERT INTO links(topic_id,link) VALUES ($1,$2) ", [req.body.topic_id, req.body.link]
+                "INSERT INTO links(topic_id,link,title) VALUES ($1,$2,$3) ", [req.body.topic_id, req.body.link,req.body.title]
             );
             res.status(200).json({})
         } catch (err) {
