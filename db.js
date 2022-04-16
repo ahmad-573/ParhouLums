fc9908dcf3b28b83243ddf8cc5f37f9a053b25bd
Fahad-Touseef <93871966+Fahad-Touseef@users.noreply.github.com>
2022-04-16 06:53:30 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const Pool = require("pg").Pool;
+const con_string = process.env.DB_STRING;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
+    con_string
+});
+
+module.exports = pool;
+
+
+/*
+user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
-    ssl: true
-});
-
-module.exports = pool;
+    sslmode: 'require'
+    */
