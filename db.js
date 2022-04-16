78d0a87eff07f5776aed8fb21bf9b863b4c97fad
Fahad-Touseef <93871966+Fahad-Touseef@users.noreply.github.com>
2022-04-16 05:45:13 +0500

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: "parhoulums"
+    database: process.env.DB_DATABASE
 });
 
 module.exports = pool;
