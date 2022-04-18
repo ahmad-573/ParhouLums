1c741b076c59fe57483118998b0ac4d3e1966cbb
Fahad-Touseef <93871966+Fahad-Touseef@users.noreply.github.com>
2022-04-18 05:19:07 +0500

    Update db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     password: 'AVNS_FQAFGBgKUByO9XH',
     host: 'db-postgresql-blr1-05163-do-user-11369164-0.b.db.ondigitalocean.com',
     port: 25060,
-    database: 'parhoulums'
+    database: 'parhoulums',
     ssl: {
       require: true, 
       rejectUnauthorized: false, 
